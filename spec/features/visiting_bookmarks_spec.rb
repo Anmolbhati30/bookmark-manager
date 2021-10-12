0d4cb1765a6500983c5b1d43aeb6005604c5b32c
Anmol <bhatianmol566@yahoo.co.uk>
2021-10-12 17:32:01 +0100

    Add test for creating new bookmark

--- spec/features/visiting_bookmarks_spec.rb
+++ spec/features/visiting_bookmarks_spec.rb
@@ -12,10 +12,27 @@ feature "Viewing bookmarks" do
     connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
     connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
 
+    test_bookmarks = Bookmark.all
+
     visit "/bookmarks/index"
 
-    some_bookmarks.each { |bookmark|
+    test_bookmarks.each { |bookmark|
       expect(page).to have_content bookmark
     }
+  end
+
+  scenario "Adding new bookmark" do
+    visit "bookmarks/index"
+
+    click_button "Create new bookmark"
+
+    # Have a form to add details about the bookmark
+    youtube_url = "http://www.youtube.com/"
+
+    fill_in :url, with: youtube_url
+    click_button "Add"
+
+    # Redirect us back to all bookmarks
+    expect(page).to have_content youtube_url
   end
 end
